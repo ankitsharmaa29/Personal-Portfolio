05f50c5a7aeed676762154fd52ffb09217fdc340
ankit sharma <as2486178@gmail.com>
2024-10-09 08:50:27 +0530

    Resume Drive Link Updated

--- src/components/Resume/Resume.tsx
+++ src/components/Resume/Resume.tsx
@@ -6,7 +6,7 @@ function Resume() {
     <>
       <div className="mt-10 flex justify-center items-center">
         <a
-          href="https://drive.google.com/file/d/1GAXYmCnRDwWkR6VtF13tgVvbNkqMwjhf/view?usp=sharing"
+          href="https://drive.google.com/file/d/1WC_m4C_2TlISM_l_BTZEdPqy8Z_vOeGW/view?usp=drive_link"
           target="_blank"
           rel="noopener noreferrer"
           className="flex items-center gap-2 py-[16px] px-8 bg-[#FEB901] hover:bg-opacity-5 hover:text-yellow-400 text-black bg-opacity-100 text-2xl rounded-full border border-yellow-400 font-semibold duration-150"
